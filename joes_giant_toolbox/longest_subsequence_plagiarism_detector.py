d2ece6d4253566ad06767154793bd68be32d9e93
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2023-04-12 13:08:00 +0200

    finished v1 longest_sentence_subsequence_plagiarism_detector()

--- joes_giant_toolbox/longest_subsequence_plagiarism_detector.py
+++ joes_giant_toolbox/longest_subsequence_plagiarism_detector.py
@@ -1,91 +1,115 @@
-import re
+from typing import List
 
 from print_progress_bar import print_progress_bar
 
 
 def longest_sentence_subsequence_plagiarism_detector(
-    ref_text: str,
-    lookup_text: str,
-    n_seq: int,
-    verbose: bool = True,
-):
+    doc1_str: str,
+    doc2_str: str,
+    min_seq_len: int,
+) -> List[str]:
     """
-    Finds the longest [n_seq] consecutive sequences of words in [lookup_text] which also appear in [ref_text]
+    Finds phrases (sequences of consecutive words) common to two documents (e.g. to act as a naive plagiarism detector)
 
-    This function was coded in a great rush and so:
-    *   Is very slow
-    *   Is very memory inefficient
-    *   Is nonetheless useful, so I have kept it
+    I had to produce this function in a short space of time so it is:
+        *   slow
+        *   memory inefficient
+        *   working reliably, even for fairly large documents
+
+    Notes
+    -----
+    The word matching is case-sensitive, so the input text should be preprocessed prior to being used in this function (e.g. all text to lower case, and possibly remove certain punctuation)
 
     Parameters
     ----------
-    TODO
+    doc1_str: str
+        The text of the first document
+    doc2_str: str
+        The text of the second document
+    min_seq_len:
+        Will only return sequences containing at least this many words
 
     Returns
     -------
-    TODO
+    List[str]
+        A list containing the phrases (sequences of consecutive words) common to both documents
+        Sorted from longest (most words) to shortest (least words)
 
     Example Usage
     -------------
     >>> longest_sentence_subsequence_plagiarism_detector(
-        ref_text="keith allen haring may 4 1958 february 16 1990 was an american artist whose pop art emerged from the new york city graffiti subculture of the 1980s1 his animated imagery has become a widely recognized visual language2 much of his work includes sexual allusions that turned into social activism by using the images to advocate for safe sex and aids awareness3 in addition to solo gallery exhibitions he participated in renowned national and international group shows such as documenta in kassel the whitney biennial in new york the são paulo biennial and the venice biennale the whitney museum held a retrospective of his art in 1997",
-        lookup_text="american artist whose art emerged from new york city graffiti subculture",
-        n_seq=3,
-    )
+    ...     doc1_str="keith allen haring may 4 1958 february 16 1990 was an american artist whose pop art emerged from the new york city graffiti subculture of the 1980s1 his animated imagery has become a widely recognized visual language much of his work includes sexual allusions that turned into social activism by using the images to advocate for safe sex and aids awareness3 in addition to solo gallery exhibitions he participated in renowned national and international group shows such as documenta in kassel the whitney biennial in new york the são paulo biennial and the venice biennale the whitney museum held a retrospective of his art in 1997",
+    ...     doc2_str="widely recognized american artist whose art emerged from new york city graffiti subculture",
+    ...     min_seq_len=2,
+    ... )
+    ['new york city graffiti subculture',
+     'american artist whose',
+     'art emerged from',
+     'widely recognized']
     """
     seq_found: list = []
-    ref_words: list = ref_text.split()
-    lookup_words: list = lookup_text.split()
-    lookup_pass_idx: dict = {}
-    progress_printer = print_progress_bar(base_message="")
+    doc1_words: list = doc1_str.split()
+    doc2_words: list = doc2_str.split()
+    common_seq_progress_printer = print_progress_bar(
+        base_message="collecting common sequences"
+    )
+    remove_subseq_progress_printer = print_progress_bar(
+        base_message="removing subsequences in result set"
+    )
 
     current_seq = []
-    for lookup_word_idx in range(len(lookup_words)):
-        progress_printer.print_progress(
-            percent_complete=(lookup_word_idx + 1) / len(lookup_words)
+    for doc2_word_idx in range(len(doc2_words)):
+        common_seq_progress_printer.print_progress(
+            percent_complete=(doc2_word_idx + 1) / len(doc2_words)
         )
-        for ref_word_idx in range(len(ref_words)):
-            if lookup_word_idx in lookup_pass_idx:
-                pass
-            elif lookup_words[lookup_word_idx] == ref_words[ref_word_idx]:
-                current_seq.append(lookup_words[lookup_word_idx])
-                lookup_pass_idx[lookup_word_idx] = None
+        for doc1_word_idx in range(len(doc1_words)):
+            if doc2_words[doc2_word_idx] == doc1_words[doc1_word_idx]:
+                current_seq.append(doc2_words[doc2_word_idx])
                 temp_add = 1
                 while 1 == 1:
                     if (
-                        lookup_word_idx + temp_add < len(lookup_words)
-                        and ref_word_idx + temp_add < len(ref_words)
-                        and lookup_words[lookup_word_idx + temp_add]
-                        == ref_words[ref_word_idx + temp_add]
+                        doc2_word_idx + temp_add < len(doc2_words)
+                        and doc1_word_idx + temp_add < len(doc1_words)
+                        and doc2_words[doc2_word_idx + temp_add]
+                        == doc1_words[doc1_word_idx + temp_add]
                     ):
-                        current_seq.append(lookup_words[lookup_word_idx + temp_add])
-                        lookup_pass_idx[lookup_word_idx + temp_add] = None
+                        current_seq.append(doc2_words[doc2_word_idx + temp_add])
                         temp_add += 1
                     else:
-                        if len(current_seq) > 1:
+                        if len(current_seq) >= min_seq_len:
                             seq_found.append(current_seq)
                         current_seq = []
                         break
 
-    return [" ".join(x) for x in sorted(seq_found, key=len, reverse=True)[:n_seq]]
+    final_seq_found: list = []
+    joined_seq_found: list = ["|".join(x) for x in seq_found]
+    for idx, seq in enumerate(seq_found):
+        remove_subseq_progress_printer.print_progress(
+            percent_complete=(idx + 1) / len(seq_found)
+        )
+        join_seq = "|".join(seq)
+        if sum([join_seq in x for x in joined_seq_found]) == 1:
+            final_seq_found.append(seq)
+
+    return [" ".join(x) for x in sorted(final_seq_found, key=len, reverse=True)]
 
 
-if __name__ == "__main__":
-    with open("/Users/josephbolton/Downloads/thesis_text", "r") as f:
-        thesis_text = f.read()
+# if __name__ == "__main__":
+#     with open("/Users/josephbolton/Downloads/thesis_text", "r") as f:
+#         thesis_text = f.read()
 
-    with open("/Users/josephbolton/Downloads/paper_text", "r") as f:
-        paper_text = f.read()
+#     with open("/Users/josephbolton/Downloads/paper_text", "r") as f:
+#         paper_text = f.read()
 
-    thesis_text = re.sub("[^\w ]", "", thesis_text).lower()
-    paper_text = re.sub("[^\w ]", "", paper_text).lower()
+#     thesis_text = re.sub("[^\w ]", "", thesis_text).lower()
+#     paper_text = re.sub("[^\w ]", "", paper_text).lower()
 
-    test = longest_sentence_subsequence_plagiarism_detector(
-        ref_text=thesis_text,
-        lookup_text=paper_text,
-        n_seq=200,
-    )
+#     test = longest_sentence_subsequence_plagiarism_detector(
+#         doc1_text=thesis_text,
+#         doc2_text=paper_text,
+#         n_seq=200,
+#     )
 
-    for x in test:
-        print(x)
-        print()
+#     for x in test:
+#         print(x)
+#         print()
